a194b3822ece17e2d9943302ff25a71f0c171ed0
Mihalescul Cosmin <cosmin.mihalescul@student.upt.ro>
2022-11-21 15:04:12 +0200

    HTML skeleton and I add list of people

---
+++ index.js
@@ -0,0 +1,11 @@
+const people = [
+    { firstName: 'Mihai', secondName: 'Enescu', dateOfBirth: '1980-01-02' },
+    { firstName: 'George', secondName: 'Ionescu', dateOfBirth: '1992-06-20' },
+    { firstName: 'Maria', secondName: 'Popescu', dateOfBirth: '1995-03-13' },
+    { firstName: 'Elena', secondName: 'Popescu', dateOfBirth: '1990-12-13' },
+    { firstName: 'Andrei', secondName: 'Ionescu', dateOfBirth: '1996-03-01' },
+    { firstName: 'Sergiu', secondName: 'Ionescu', dateOfBirth: '1990-02-01' }
+];
+
+
+console.log(people);
